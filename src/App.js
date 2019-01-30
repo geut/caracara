1a0433639b83cabf6bd716269706a7a4c717a6fb
Diego Paez <paez.di@gmail.com>
2019-01-29 21:49:22 -0300

    Update: initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,200 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Modal from 'react-modal';
+
+// P2P stuff
+import ram from 'random-access-memory';
+import swarm from '@geut/discovery-swarm-webrtc';
+import signalhub from 'signalhubws';
+import Saga from './p2p/core';
+import config from './p2p/config';
+
+// internal components
+import MarkdownPreview from './Editor';
 import './App.css';
 
-class App extends Component {
+// P2P DEFAULTS
+const webrtcOpts = {
+  config: {
+    iceServers: (process.env.ICE_URLS || config.ICE_URLS).split(';').map(data => {
+      const [urls, credential, username] = data.split(',')
+
+      if (credential && username) {
+        return {
+          urls,
+          credential,
+          username
+        }
+      }
+
+      return { urls }
+    })
+  }
+};
+
+// END P2P DEFAULTS
+
+const initComm = async (username, key) => {
+  const publicKey = key && key.length > 0 ? key : null;
+  //username = Date.now().toString();
+  const saga = Saga(ram, publicKey, username);
+
+  await saga.initialize();
+
+  const sw = swarm({
+    id: username,
+    stream: () => {
+      return saga.replicate()
+    }
+  });
+
+
+  const discoveryKey = saga.db.discoveryKey.toString('hex');
+  const signalUrls = (process.env.SIGNAL_URLS || config.SIGNAL_URLS).split(';');
+  sw.join(signalhub(discoveryKey, signalUrls), webrtcOpts);
+
+  sw.on('connection', async peer => {
+    try {
+      await saga.connect(peer);
+    } catch (err) {
+      console.log(err);
+    }
+  })
+
+  return saga;
+}
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
+class MdDoc extends Component {
+  constructor() {
+    super();
+    this.comm = null;
+    this.state = {
+      peerValue: {
+        text: ''
+      },
+      localHistory: []
+    }
+  }
+
+  async componentDidUpdate(prevProps, prevState) {
+    const { username, draftId } = this.props;
+    if (username !== prevProps.username) {
+      // username has been set
+      this.comm = await initComm(username, draftId);
+      // connect the listener
+      this.comm.on('message', data => {
+        console.log('new message arrival from:', data.username)
+        console.log('new message arrival content:', data.message)
+        console.log('message history:', data.history)
+        //if (this.props.username !== data.username){
+        this.setState({ peerValue: data.message.peerValue, localHistory: [ ...this.state.localHistory, data.history[0] ]});
+        //}
+      })
+    }
+  }
+
+  updatePeerValue = val => {
+    console.log('updatePeerValue', val)
+    this.setState({ peerValue: val }, () => {
+      this.comm.writeMessage({ peerValue: val, username: this.props.username });
+    });
+  }
+
+  componentWillUnmount() {
+    this.comm.removeAllListeners('message')
+  }
+
   render() {
+    const { username, draftId } = this.props;
+    return (
+      <>
+        <header className="App-header">
+          Caracara <span role="img" aria-label="caracara bird using a emoji">🐧</span>
+          <span className="App-username">{username ? `${username} is online` : ''}</span>
+          <div>
+              {this.comm && this.comm.db.key ? `Draft: ${this.comm.db.key.toString('hex')}` : ''}
+          </div>
+        </header>
+        <div className="App-editor">
+          <MarkdownPreview peerValue={this.state.peerValue} updatePeerValue={this.updatePeerValue}/>
+        </div>
+        <aside className="App-history">
+          History:
+          <ul>
+              {this.state.localHistory.reverse().map(h => <li>{h[0]}</li>)}
+          </ul>
+        </aside>
+      </>
+    )
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      modalIsOpen: true,
+      username: ''
+    };
+
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.params = new URLSearchParams(window.location.search);
+
+    this.draftId = this.params.get('draft');
+  }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+  }
+
+  saveUsername = (e) => {
+    const { target } = e;
+    this.setState({ username: target.value });
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
+  render(props) {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Set your username"
+        >
+          <form>
+            <legend>Set your username</legend>
+            <input type="text" onChange={this.saveUsername}/>
+          </form>
+          <button onClick={this.closeModal}>Enter</button>
+        </Modal>
+        <Router>
+          <Route path="/" render={props => {
+            return <MdDoc username={this.state.username} draftId={this.draftId} />
+          }} />
+        </Router>
       </div>
     );
   }
---
+++ src/p2p/core.js
@@ -0,0 +1,191 @@
+const Automerge = require('automerge')
+const EventEmitter = require('events')
+const { Writable } = require('stream')
+const hyperdb = require('hyperdb')
+const pump = require('pump')
+const hyperid = require('hyperid')
+const uuid = hyperid()
+
+class ForEachChunk extends Writable {
+  constructor (opts, cb) {
+    if (!cb) {
+      cb = opts
+      opts = {}
+    }
+    super(opts)
+
+    this.cb = cb
+  }
+
+  _write (chunk, enc, next) {
+    this.cb(chunk, enc, next)
+  }
+}
+
+const forEachChunk = (...args) => new ForEachChunk(...args)
+
+class Saga extends EventEmitter {
+  constructor (storage, key, username) {
+    super()
+
+    this.operations = new Map()
+    this.newestOperations = []
+    this.users = new Map()
+    this.username = username
+    this.timestamp = Date.now()
+    this.doc = Automerge.init()
+
+    this.db = hyperdb(storage, key, { valueEncoding: 'json' })
+  }
+
+  async initialize () {
+    await this._ready()
+
+    this._updateHistory(this._watchForOperations.bind(this))
+  }
+
+  // TODO(dk); rename to writeOperation
+  writeMessage (message) {
+    const key = `operations/${uuid()}`
+    // NOTE(dk): operation can be part of message. Maybe message can be seen as content receipt? but thus would make an op like a 2nd class citizen and we dont want to transmit that :thinking:
+    const data = {
+      key,
+      message,
+      username: this.username,
+      timestamp: Date.now()
+    }
+
+    return new Promise((resolve, reject) => {
+      this.db.put(key, data, (err) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(key)
+        }
+      })
+    })
+  }
+
+  replicate () {
+    return this.db.replicate({
+      live: true,
+      userData: JSON.stringify({
+        key: this.db.local.key,
+        username: this.username,
+        timestamp: this.timestamp
+      })
+    })
+  }
+
+  async connect (peer) {
+    if (!peer.remoteUserData) {
+      throw new Error('peer does not have userData')
+    }
+
+    const data = JSON.parse(peer.remoteUserData)
+
+    const key = Buffer.from(data.key)
+    const username = data.username
+
+    await this._authorize(key)
+
+    if (!this.users.has(username)) {
+      this.users.set(username, new Date())
+      this.emit('join', data)
+      peer.on('close', () => {
+        if (!this.users.has(username)) return
+        this.users.delete(username)
+        this.emit('leave', data)
+      })
+    }
+  }
+
+  _authorize (key) {
+    return new Promise((resolve, reject) => {
+      this.db.authorized(key, (err, auth) => {
+        if (err) return reject(err)
+
+        if (auth) {
+          return resolve()
+        }
+
+        this.db.authorize(key, (err) => {
+          if (err) return reject(err)
+          resolve()
+        })
+      })
+    })
+  }
+
+  _updateHistory (onFinish) {
+    const h = this.db.createHistoryStream({ reverse: true })
+
+    // reset newestOperations
+    this.newestOperations = []
+
+    const ws = forEachChunk({ objectMode: true }, (data, enc, next) => {
+      const { key, value } = data
+
+      if (/operations/.test(key)) {
+        console.log(`check if operations has key ${key}`)
+        if (this.operations.has(key)) {
+          console.log(`Destroying history stream - operation found with key ${key}`)
+          //h.destroy()
+            //return
+        } else {
+          console.log(`Adding new operation key ${key}`)
+          this.newestOperations.push(data)
+        }
+      }
+
+      next()
+    })
+
+    pump(h, ws, err => {
+      // work with latest operations in the right order
+      const opsLength = this.newestOperations.length;
+      this.newestOperations.reverse().map((op, idx) => {
+        console.log('aplicando operaciones nuevas')
+        const { key, value } = op;
+        const { message } = value;
+        const { peerValue } = message;
+        const { text, operation } = peerValue;
+        const historyMessage = `${value.username} ${operation === 'DELETE' ? 'removed' : 'added'} some text - ⌚️ ${value.timestamp}`;
+        const newDoc = Automerge.change(this.doc,
+          historyMessage,
+          doc => {
+            doc.text = new Automerge.Text()
+            doc.text.insertAt(0, value.message.peerValue.text)
+            return doc
+          }
+        )
+        const history = Automerge.getHistory(newDoc).map(state => [state.change.message, state.snapshot.text])
+        this.emit('message', { ...value, text: newDoc.text.join(''), history }, key)
+        // write latest operation
+        /*
+        if (idx === 0) {
+          console.log(`last known operation key ${op.key}`)
+          console.log(`last known operation text ${op.value.message.peerValue.text}`)
+          this.operations.set(op.key, op.value);
+        }
+        */
+        this.operations.set(op.key, op.value);
+      })
+
+      if (onFinish) onFinish(err)
+    })
+  }
+
+  _watchForOperations() {
+    this.db.watch('operations', () => {
+      this._updateHistory()
+    })
+
+  }
+
+  _ready () {
+    return new Promise(resolve => this.db.ready(resolve))
+  }
+}
+
+export default (...args) => new Saga(...args)
