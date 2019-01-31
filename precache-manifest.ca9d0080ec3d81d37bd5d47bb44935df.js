dfff817ce522d2b28439ee8ebdd2d56098429609
Diego Paez <paez.di@gmail.com>
2019-01-31 14:36:43 -0300

    Updates

---
+++ precache-manifest.ca9d0080ec3d81d37bd5d47bb44935df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "542016a8469e89ac0a5b",
+    "url": "/caracara/static/js/runtime~main.542016a8.js"
+  },
+  {
+    "revision": "2930b6060e337cb7397a",
+    "url": "/caracara/static/js/main.2930b606.chunk.js"
+  },
+  {
+    "revision": "0a00807d99a572c0fea4",
+    "url": "/caracara/static/js/1.0a00807d.chunk.js"
+  },
+  {
+    "revision": "2930b6060e337cb7397a",
+    "url": "/caracara/static/css/main.0f236fec.chunk.css"
+  },
+  {
+    "revision": "12153b0b6451c4ea514b0d62b5c92b32",
+    "url": "/caracara/index.html"
+  }
+];
